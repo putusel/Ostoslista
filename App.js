f431a10b41d222a92bdd163c31fc0d27e6a73a17
putusel <elina.putus@gmail.com>
2022-09-03 21:48:14 +0300

    Tehtävä lähes valmis

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, TextInput, Button, FlatList, Image } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => { 
+    setData([...data, { key: text }]);
+    setText('');
+  }
+  const buttonPressed2 = () => { 
+    const data = '';
+    setData([...data, { key: text }]);
+    setText('');
+  }
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={{uri: 'https://th.bing.com/th/id/OIP.gxvUa1_2vWnkr7qiWkQ2HgHaIm?w=150&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7'}} />
+      
+        <TextInput 
+          style={styles.input} 
+          placeholder="enter an item"
+          onChangeText={text => setText(text)} value={text}
+        />
+        <View style={{ width:Dimensions.get("window").width * 0.4, flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginBottom: 10 }}>
+          <Button onPress={buttonPressed} title="ADD" />
+          <Button onPress={buttonPressed2} title="CLEAR" />
+        </View>
+        <Text style={styles.text}>Shopping List</Text>
+        <FlatList style={styles.list}
+          data={data}
+          renderItem={({ item }) =>
+            <Text>{item.key}</Text>
+        }/>
+       <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,4 +48,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image : {
+    width: 250,
+    height: 100,
+    marginTop: 10,
+    marginBottom: 10
+    
+  },
+  input : {
+    width:200, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    fontSize: 20,
+  },
+  text : {
+    fontSize: 20,
+    color: '#0000ff',
+}
 });
