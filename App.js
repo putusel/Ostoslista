d77e061747ce54a2f3ed015c4d70e9600eba8f04
putusel <elina.putus@gmail.com>
2022-09-04 16:30:04 +0300

    Tehtövö valmis

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, TextInput, Button, FlatList, Image } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, Image, Dimensions, FlatList } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('');
@@ -16,27 +17,22 @@ export default function App() {
     setText('');
   }
 
+
   return (
-    
     <View style={styles.container}>
       <Image style={styles.image} source={{uri: 'https://th.bing.com/th/id/OIP.gxvUa1_2vWnkr7qiWkQ2HgHaIm?w=150&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7'}} />
-      
-        <TextInput 
-          style={styles.input} 
-          placeholder="enter an item"
-          onChangeText={text => setText(text)} value={text}
-        />
-        <View style={{ width:Dimensions.get("window").width * 0.4, flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginBottom: 10 }}>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
+      <View style={{ width:Dimensions.get("window").width * 0.4, flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginBottom: 20}}>
           <Button onPress={buttonPressed} title="ADD" />
           <Button onPress={buttonPressed2} title="CLEAR" />
-        </View>
-        <Text style={styles.text}>Shopping List</Text>
-        <FlatList style={styles.list}
+      </View>
+      <Text style={styles.text}>Shopping List</Text>
+      <FlatList style={styles.list}
           data={data}
           renderItem={({ item }) =>
             <Text>{item.key}</Text>
         }/>
-       <StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -51,18 +47,19 @@ const styles = StyleSheet.create({
   image : {
     width: 250,
     height: 100,
-    marginTop: 10,
-    marginBottom: 10
-    
+    marginTop: 10, 
+    marginBottom: 10,
   },
   input : {
     width:200, 
     borderColor: 'gray', 
     borderWidth: 1,
-    fontSize: 20,
+    marginTop: 10, 
+    marginBottom: 20,
   },
   text : {
     fontSize: 20,
     color: '#0000ff',
-}
+  }
+
 });
